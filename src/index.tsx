3017ef04046b5dd44352d50b8ad882212c9ccd4c
Ali Balouchi <alibalouchi.74@gmail.com>
2022-08-27 08:41:24 -0400

    fix publishing issues

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ModalProvider } from 'global-modal';
 
 const root = ReactDOM.createRoot(
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ModalProvider>
         <App />
       </ModalProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
