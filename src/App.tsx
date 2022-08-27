0f21bf3475d955e204815a7684a2141f15c49b11
Ali Balouchi <alibalouchi.74@gmail.com>
2022-08-27 09:08:49 -0400

    fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Collection from './pages/Collection';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <HashRouter basename='pronounce-app'>
+    <Routes>
       {/* <Route path='/pronounce-app' element={<Navigate to="/" />} /> */}
-      <Route path='/' element={<Home />} />
-      <Route path='/collection/:collectionName' element={<Collection />} />
-    </HashRouter>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection/:collectionName' element={<Collection />} />
+    </Routes>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ModalProvider>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </ModalProvider>
   </React.StrictMode>
 );
