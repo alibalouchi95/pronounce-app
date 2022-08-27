9396c39bac0bd6f229ffea947769bf4389f51542
Ali Balouchi <alibalouchi.74@gmail.com>
2022-08-27 11:15:33 -0400

    remove warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Collection from './pages/Collection';
 import Home from './pages/Home';
